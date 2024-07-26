b234f32d41885e3418578d01c1b2feaf5f9b3c43
schroederlab <sylvia_schroeder@outlook.com>
2024-07-26 19:46:31 +0100

    aligning traces

--- +spatial/plotPrefDiffVsDist.m
+++ +spatial/plotPrefDiffVsDist.m
@@ -15,45 +15,54 @@ function fig = plotPrefDiffVsDist(distances, prefDiffs, prefDiffsPermuted, ...
 % fig       int, handle to figure
 
 fig = [];
-minSamples = 6;
+minSamples = 6; % minimum number of pairs per bin for moving average
 
 numPerm = size(prefDiffsPermuted,2);
-
 binCentres = 0:stepSize:max(distances);
 binValid = true(length(binCentres),1);
 meanDist = NaN(length(binCentres),1);
 meanDiff = NaN(length(binCentres),1);
-semDiff = NaN(length(binCentres),1);
 meanDiffPerm = NaN(length(binCentres), numPerm);
+% loop over bins to determine moving average (of tuning differences and
+% brain distances)
 for bin = 1:length(binCentres)
     ind = distances > binCentres(bin)-binSize/2 & ...
         distances <= binCentres(bin)+binSize/2;
     meanDist(bin) = mean(distances(ind));
+    % check that bin contains sufficient samples
     if sum(ind) < minSamples
         binValid(bin) = false;
     end
 
     meanDiff(bin) = mean(prefDiffs(ind), 'omitnan');
-    semDiff(bin) = std(prefDiffs(ind), 'omitnan') ./ sqrt(sum(ind));
     meanDiffPerm(bin,: ) = mean(prefDiffsPermuted(ind,:), 1, 'omitnan');
 end
+% if not enough data, return
 if sum(binValid) < 2
     return
 end
+% ignore bins with insufficient pairs
 meanDist = meanDist(binValid);
 meanDiff = meanDiff(binValid);
+% determine median and confidence interval of null distribution (permuted
+% data)
 meanDiffNull = prctile(meanDiffPerm(binValid,:), [2.5 50 97.5], 2);
+% interpolate data to equal-distant brain distances
 x = ceil(meanDist(1):2:floor(meanDist(end)));
 y = interp1(meanDist, meanDiff, x);
-ySm = smoothdata(y, 'gaussian', 7);
 yNull = interp1(meanDist, meanDiffNull, x);
+% smooth moving averages
+ySm = smoothdata(y, 'gaussian', 7);
 yNullSm = smoothdata(yNull, 1, 'gaussian', 7);
 
 h = [0 0];
 fig = figure;
+% scatterplot
 plot(distances, prefDiffs, 'k.')
 hold on
+% moving average of real data
 h(1) = plot(x, ySm, 'r-', 'LineWidth', 2);
+% moving average of null data and confidence interval
 fill([x flip(x)], [yNullSm(:,1); flip(yNullSm(:,3))], 'b', ...
     'FaceAlpha', 0.3, 'EdgeColor', 'none')
 h(2) = plot(x, yNullSm(:,2), 'b-', 'LineWidth', 2);
---
+++ +traces/getAlignedTraces.m
@@ -0,0 +1,39 @@
+function [aligned, t] = getAlignedTraces(trace, time, eventTimes, window)
+% aligned   % [t x trials x numSignals]
+
+aligned = [];
+t = [];
+
+dt = median(diff(time));
+w = round(window / dt);
+mini = min(w(:,1));
+maxi = max(w(:,2));
+tmp = mini : maxi;
+if size(window,1) == 1
+    tInd = repmat(tmp, length(eventTimes), 1);
+else
+    if size(window,1) ~= length(eventTimes)
+        disp('No. events and windows have to be the same!')
+        return
+    end
+    tInd = repmat(tmp, size(window,1), 1);
+    for j = 1:size(window,1)
+        tInd(j, tmp<w(j,1) | tmp>w(j,2)) = NaN;
+    end
+end
+t = tmp .* dt;
+
+aligned = NaN(length(t), length(eventTimes), size(trace, 2));
+
+for ev = 1:length(eventTimes)
+    evInd = find(time > eventTimes(ev), 1);
+    if isempty(evInd)
+        continue
+    end
+    if evInd>1 && eventTimes(ev)-time(evInd-1) < time(evInd)-eventTimes(ev)
+        evInd = evInd - 1;
+    end
+    inds = evInd + tInd(ev,:);
+    valid = inds > 0 & inds <= length(time);
+    aligned(valid,ev,:) = trace(inds(valid),:);
+end
