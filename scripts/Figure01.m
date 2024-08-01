600e446a6f60a8459e1822e547cf492c3c7bd165
schroederlab <sylvia_schroeder@outlook.com>
2024-08-01 14:48:01 +0100

    Fig 1: example boutons

--- scripts/Figure01.m
+++ scripts/Figure01.m
@@ -2,10 +2,19 @@
 getFolders;
 
 %% Parameters
+sets = {'boutons', 'neurons'};
+maxP = 0.05; % p-value threshold for direction/orientation selectivity
 
 %% Examples
 ex = cell(2,4); % rows: (1) bouton, (2) neuron
-ex(1,:) = {'SS078', '2017-09-28', 1, []};
+% good boutons
+% ex(1,:) = {'SS078', '2017-09-28', 1, [1 3 4 5 6 8 9 10 12 14 15 16 17 18 19 20 21 23 24 26 27 29 31 32 33 34 35 36 38 39 40 42 45 46 48 49 50 51 52 57 58 60 62 63 64 71 72 74 80 83 95 99 100 105 107 109 110 123 125 126 128 135 142 145 146 150 165 168 175 176 180 181 186 187 192 196 200 201 205 207 208 217 224 229 239 242 252 257 262 269 276]};
+% final selection
+% ex(1,:) = {'SS078', '2017-09-28', 1, [40 46 48  51 62 192  42  74 205]};
+ex(1,:) = {'SS078', '2017-09-28', 1, [40 48  51 62 192  42]};
+% best OS boutons
+% ex(1,:) = {'SS078', '2017-09-28', 1, [18 45 150]};
+% other good datasets
 % ex(1,:) = {'SS078', '2017-10-05', 1, []};
 % ex(1,:) = {'SS077', '2017-10-03', 1, []};
 % good neurons
@@ -18,19 +27,20 @@ ex(1,:) = {'SS078', '2017-09-28', 1, []};
 % ex(2,:) = {'SS044', '2015-04-28', 3, [227 236 240 285 383 408 425 426 430]};
 % final selection (2 triples of neighbours)
 ex(2,:) = {'SS044', '2015-04-28', 3, [328 378 369 236 245 258]};
-sets = {'boutons', 'neurons'};
 
 %% Add paths
 addpath(genpath(fullfile(folders.tools, 'npy-matlab')))
 addpath(fullfile(folders.repo))
 
-%% Example mean frames, calcium traces and tuning curves
-buffer = 1; % in sec (before and after stim period)
+%% For all plots
 fPlot = fullfile(folders.plots, 'Figure01');
 if ~isfolder(fPlot)
     mkdir(fPlot)
 end
-for s = 2
+
+%% Example mean frames, calcium traces and tuning curves
+buffer = 1; % in sec (before and after stim period)
+for s = 1:2
     str = sets{s};
     f = fullfile(folders.data, str, ex{s,1}, ex{s,2});
     calc = io.getCalciumData(f);
@@ -152,6 +162,25 @@ for s = 2
 end
 
 %% Population direction tuning curves
+for s = 1:2 % neurons and boutons
+    subjDirs = dir(fullfile(folders.data, sets{s}, 'SS*'));
+    oriCurves = [];
+    dirCurves = [];
+    stimAll = 0:30:330;
+    for subj = 1:length(subjDirs) % animals
+        name = subjDirs(subj).name;
+        dateDirs = dir(fullfile(folders.data, sets{s}, name, '2*'));
+        for dt = 1:length(dateDirs) %dates
+            date = dateDirs(dt).name;
+            f = fullfile(folders.data, sets{s}, name, date);
+
+            stim = io.getGratingInfo(f, 'gratingsDrifting');
+            krnlFits = io.getStimResponseFits(f, 'gratingsDrifting');
+            [dirTuning, oriTuning] = io.getTuningResults(f, 'gratingsDrifting');
+
+        end
+    end
+end
 
 %% Population orientation tuning curves
 
