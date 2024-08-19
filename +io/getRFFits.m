f95255c6cade8ea564f1df50fab3d49cc07e8781
schroederlab <sylvia_schroeder@outlook.com>
2024-08-19 19:06:18 +0100

    Comments

--- +io/getRFFits.m
+++ +io/getRFFits.m
@@ -1,4 +1,32 @@
 function data = getRFFits(f)
+%GETRFFITS   Load receptive fields fitted to responses to visual noise.
+
+% INPUTS
+% folder            path to data of recording session
+
+% OUTPUTS
+% data
+%   .maps           [ROIs x rows x columns x t_rf x type], spatio-temporal
+%                   RF for ON (type=1) and OFF (type=2) subfields
+%   .types          {ROIs}, 'ON', 'OFF', or 'ON+OFF'
+%   .fitParameters  [ROIs x parameters], (amplitude, xCenter, xSTD,
+%                   yCenter, ySTD, rotation)
+%   .peaks          [ROIs], amplitude of RF peak (of best type) in STD of
+%                   noise
+%   .gaussMasks     [ROIs x rows x columns x type], best fitting Gaussian
+%                   mask for ON (type=1) and OFF (type=2) subfields, if RF
+%                   is not ON+OFF, only the subfield of the RF type is
+%                   non-zero
+%   .timeWeights    [ROIs x t_rf], optimal weights for fitted Gaussian
+%                   spatial RF to match spatio-temporal RF (map)
+%   .EV             [ROIs], explained variance of trace prediction based on
+%                   Gaussian map (gaussMasks) * temporal weights
+%                   (timeWeights)
+%   .predictions    [t x ROIs], trace predictions based on Gaussian map
+%                   (gaussMasks) * temporal weights (timeWeights)
+%   .time_prediction [t], sample times of trace predictions
+%   .edges          [left rigth top bottom] of RF map
+%   .time_RF        [t_rf], sample times of spatio-temporal RFs
 
 data.maps = readNPY(fullfile(f, '_ss_rf.maps.npy'));
 data.types = readcell(fullfile(f, '_ss_rf.type.csv'));
