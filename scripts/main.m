059d6e95447580728cf1ef1fd5e7d96ecf79150c
Sylvia Schröder <sylvia_schroeder@outlook.com>
2025-09-18 20:35:45 +0200

    Plotting ephys RFs

--- scripts/main.m
+++ scripts/main.m
@@ -5,21 +5,21 @@
 % folders.repo:     current repository
 % folders.plots:    to save plots
 
-% % Mac
-% folders.data = '/Users/ss2302/Library/CloudStorage/OneDrive-UniversityofSussex/Projects/2023_OrientationColumns/DataToPublish';
-% folders.results = '/Users/ss2302/Library/CloudStorage/OneDrive-UniversityofSussex/Projects/2023_OrientationColumns/Results_Sylvia';
-% folders.tools = '/Users/ss2302/dev/toolboxes';
-% folders.repo = '/Users/ss2302/dev/he_schroeder_columns';
-% folders.plots = '/Users/ss2302/Documents/Results/OrientationColumns_Plots';
+% Mac
+folders.data = '/Users/ss2302/Library/CloudStorage/OneDrive-UniversityofSussex/Projects/2023_OrientationColumns/DataToPublish';
+folders.results = '/Users/ss2302/Library/CloudStorage/OneDrive-UniversityofSussex/Projects/2023_OrientationColumns/Results_Sylvia';
+folders.tools = '/Users/ss2302/dev/toolboxes';
+folders.repo = '/Users/ss2302/dev/he_schroeder_columns';
+folders.plots = '/Users/ss2302/Library/CloudStorage/OneDrive-UniversityofSussex/Projects/2023_OrientationColumns/Results_Sylvia';
 
-% PC on campus
-folders.data = 'C:\Users\Sylvia\OneDrive - University of Sussex\Projects\2023_OrientationColumns\DataToPublish';
-folders.dataRawEphys = 'Z:\RawData';
-% folders.results = 'D:\Results\OrientationColumns';
-folders.results = 'C:\Users\Sylvia\OneDrive - University of Sussex\Projects\2023_OrientationColumns\Results_Sylvia';
-folders.tools = 'C:\dev\toolboxes';
-folders.repo = 'C:\dev\workspaces\he_schroeder_columns';
-folders.plots = 'D:\Results\OrientationColumns_Plots';
+% % PC on campus
+% folders.data = 'C:\Users\Sylvia\OneDrive - University of Sussex\Projects\2023_OrientationColumns\DataToPublish';
+% folders.dataRawEphys = 'Z:\RawData';
+% % folders.results = 'D:\Results\OrientationColumns';
+% folders.results = 'C:\Users\Sylvia\OneDrive - University of Sussex\Projects\2023_OrientationColumns\Results_Sylvia';
+% folders.tools = 'C:\dev\toolboxes';
+% folders.repo = 'C:\dev\workspaces\he_schroeder_columns';
+% folders.plots = 'D:\Results\OrientationColumns_Plots';
 
 %% Add paths
 addpath(genpath(fullfile(folders.tools, 'npy-matlab')))
