155438dd7d174e0925a410f413fbd2ad1cc1ed4b
Sylvia Schröder <sylvia_schroeder@outlook.com>
2025-10-03 17:34:21 +0100

    Update getEphysData.m

--- +io/getEphysData.m
+++ +io/getEphysData.m
@@ -12,8 +12,13 @@ function data = getEphysData(folder)
 %   .amps
 %   .clusters
 %   .depths
+%   .clusterIDs
 
 data.times = readNPY(fullfile(folder, "spikes.times.npy"));
 data.amps = readNPY(fullfile(folder, "spikes.amps.npy"));
 data.clusters = readNPY(fullfile(folder, "spikes.clusters.npy"));
 data.depths = readNPY(fullfile(folder, "spikes.depths.npy"));
+data.clusterIDs = readNPY(fullfile(folder, "clusters.ids.npy"));
+if isfile(fullfile(folder, "clusters.depths.npy"))
+    data.clusterDepths = readNPY(fullfile(folder, "clusters.depths.npy"));
+end
