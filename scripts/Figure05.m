351f23ba7b7e31342c5192136876938d8b271c95
Sylvia Schröder <sylvia_schroeder@outlook.com>
2025-11-03 17:39:25 +0000

    Corrected orientation vectors

--- scripts/Figure05.m
+++ scripts/Figure05.m
@@ -17,18 +17,21 @@ end
 [ds_trans, os_long, os_lat] = algebra.getDsOsAxes();
 % Cartoon of longitudes for direction preferences
 % average body axis
-body = mean([ds_trans(1,:); (ds_trans(2,:)+[180 0]).*[1 -1]], 1);
-gravitation = mean([ds_trans(3,:); (ds_trans(4,:)+[180 0]).*[1 -1]], 1);
+body = mean( ...
+    [mod(ds_trans(1,1), 360), ds_trans(1,2); ...
+     mod(ds_trans(2,1)+180, 360), -ds_trans(2,2)], 1);
+gravitation = mean( ...
+    [mod(ds_trans(3,1), 360), ds_trans(3,2); ...
+     mod(ds_trans(4,1)+180, 360), -ds_trans(4,2)], 1);
 
 figure('Position', glob.figPositionDefault)
 hold on
 [X,Y,Z] = sphere(100);
 surf(X, Y, Z, 'FaceColor', 'w', 'FaceAlpha', 1, 'EdgeColor', 'none')
-algebra.plotLongitudes(body, 12, 'b')
-algebra.plotLongitudes(gravitation, 12, 'c')
+algebra.plotLongitudes(body, 12, 'c')
+algebra.plotLongitudes(gravitation, 12, 'b')
 algebra.plotLatitudes([0 90], 1, 'k')
 algebra.plotLatitudes([0 0], 1, 'k')
-% view([-62, 25])
 view([118, 25])
 io.saveFigure(gcf, fPlots, 'cartoon_directionVectors');
 
@@ -36,8 +39,8 @@ figure('Position', glob.figPositionDefault)
 hold on
 [X,Y,Z] = sphere(100);
 surf(X, Y, Z, 'FaceColor', 'w', 'FaceAlpha', 1, 'EdgeColor', 'none')
-algebra.plotLongitudes(os_long(1,:), 12, 'b')
-algebra.plotLongitudes(os_long(2,:), 12, 'c')
+algebra.plotLongitudes(os_long(1,:), 12, 'c')
+algebra.plotLongitudes(os_long(2,:), 12, 'b')
 algebra.plotLatitudes([0 90], 1, 'k')
 algebra.plotLatitudes([0 0], 1, 'k')
 view([118, 25])
@@ -47,8 +50,8 @@ figure('Position', glob.figPositionDefault)
 hold on
 [X,Y,Z] = sphere(100);
 surf(X, Y, Z, 'FaceColor', 'w', 'FaceAlpha', 1, 'EdgeColor', 'none')
-algebra.plotLatitudes(os_lat(1,:), 8, 'r')
-algebra.plotLatitudes(os_lat(2,:), 8, 'y')
+algebra.plotLatitudes(os_lat(1,:), 8, 'm')
+algebra.plotLatitudes(os_lat(2,:), 8, 'r')
 algebra.plotLatitudes([0 90], 1, 'k')
 algebra.plotLatitudes([0 0], 1, 'k')
 view([118, 25])
