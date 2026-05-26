bf183c1fe6de0506377c701b78a67e29d049a89d
Sylvia Schröder <sylvia_schroeder@outlook.com>
2026-05-26 17:02:26 +0100

    Add edge-distance filtering and dist2edge param
    Update selectRFStim to inlcude only units with RFs far enough from monitor edge.

--- +rf/selectRFStim.m
+++ +rf/selectRFStim.m
@@ -1,9 +1,11 @@
-function stimTypes = selectRFStim(results, minEV, minPeak)
+function stimTypes = selectRFStim(results, minEV, minPeak, dist2edge)
 %SELECTRFSTIM   For each unit, select stimulus (visual noise or cirlces) 
 % resulting in higher explained variance.
 
 % INPUTS
 % results   {2}, entries for two stimulus types
+%   .gaussPars      [ROIs x parameters], (amplitude, xCenter, xSTD,
+%                   yCenter, ySTD, rotation, offset)
 %   .peaks  [units], amplitude of RF peak (of best type) in STD of
 %           noise
 %   .EVs    [units], explained variance of trace prediction based on
@@ -16,6 +18,9 @@ function stimTypes = selectRFStim(results, minEV, minPeak)
 % stimTypes [units], 1: if 1st stimulus is better, 2: if 2nd stimulus is
 %           better, NaN: if no stimulus yielded significant RF
 
+edges = [-95 0 51 -27]; % [left, right, top, bottom] where bottom
+                        % and top are negative if above horizon
+
 n = 0;
 for stimType = 1:2
     if isempty(results{stimType})
@@ -25,9 +30,15 @@ for stimType = 1:2
         n = length(results{stimType}.peaks);
         EVs = NaN(n, 2);
     end
-    good = results{stimType}.EV >= minEV & ...
-        results{stimType}.peaks >= minPeak;
-    EVs(good, stimType) = results{stimType}.EV(good);
+    dt = results{stimType};
+    pos = results{stimType}.gaussPars(:,[2 4]);
+    % exclude all RFs too close to monitor edge
+    invalidRF = dt.EV < minEV | dt.peaks < minPeak | ...
+        pos(:,1) < edges(1)+dist2edge | ...
+        pos(:,1) > edges(2)-dist2edge | ...
+        pos(:,2) > edges(3)-dist2edge | ...
+        pos(:,2) < edges(4)+dist2edge;
+    EVs(~invalidRF, stimType) = results{stimType}.EV(~invalidRF);
 end
 [~, stimTypes] = max(EVs, [], 2);
 stimTypes(all(isnan(EVs), 2)) = NaN;
