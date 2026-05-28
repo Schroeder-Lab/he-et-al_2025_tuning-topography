f082eb0dbaeb6e731a4c2790ec0cdb84306d2e7d
Sylvia Schröder <sylvia_schroeder@outlook.com>
2026-05-28 09:50:59 +0100

    Add getPupilData loader
    Add +io/getPupilData.m to load pupil tracking data.

---
+++ +io/getPupilData.m
@@ -0,0 +1,13 @@
+function data = getPupilData(folder)
+%GETPUPILDATA   Load pupil data.
+
+% INPUTS
+% folder    path to data of recording session
+
+% OUTPUTS
+% data
+%   .xyPos  [time x 1], coordinates of pupil centre (from top left of video)
+%   .time   [time x 1], sampling times of pupil trace
+
+data.xyPos = readNPY(fullfile(folder, 'eye.timestamps.npy'));
+data.time = readNPY(fullfile(folder, 'eye.timestamps.npy'));
